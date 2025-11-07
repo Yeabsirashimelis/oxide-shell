c2d5f4402ecf48604e54ae7dbd886f7846594c4a
Yeabsirashimelis <shimelisyeabsiragithub@gmail.com>
2025-11-06 23:30:25 -0800

    moving to home directory implemented for linux

--- src/shell/commands.rs
+++ src/shell/commands.rs
@@ -158,19 +158,31 @@ fn run_pwd_command() {
 }
 
 fn run_cd_command(path_str: &str) {
-    let mut target_path = PathBuf::new();
-
-    if path_str == "~" {
-        if let Result::Ok(home) = env::var("USERPROFILE") {
-            target_path.push(home);
+    let target_path = if path_str.trim().is_empty() || path_str == "~" {
+        // Try HOME first (Linux)
+        if let Ok(home) = env::var("HOME") {
+            PathBuf::from(home)
+            //THEN USERPROFILE (windows)
+        } else if let Ok(home) = env::var("USERPROFILE") {
+            PathBuf::from(home)
+        } else {
+            PathBuf::from("/")
         }
+    } else if path_str.starts_with("~/") {
+        let mut home = if let Ok(home) = env::var("HOME") {
+            PathBuf::from(home)
+        } else if let Ok(home) = env::var("USERPROFILE") {
+            PathBuf::from(home)
+        } else {
+            PathBuf::from("/")
+        };
+        home.push(&path_str[2..]);
+        home
     } else {
-        target_path.push(path_str);
-    }
+        PathBuf::from(path_str)
+    };
 
-    let dir_change = env::set_current_dir(target_path);
-
-    if let Result::Err(_) = dir_change {
+    if let Err(_) = env::set_current_dir(&target_path) {
         eprintln!("cd: {}: No such file or directory", path_str);
     }
 }
